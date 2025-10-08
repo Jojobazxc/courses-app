733d23f8abb4ce99937e3fedc8df230ea31eb5fd
Jojobazxc <piperov2004@mail.ru>
2025-10-08 14:45:26 +0300

    add auth screen without UI validation and navigation to main fragment

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -51,4 +51,7 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.57.1")
     kapt("com.google.dagger:hilt-compiler:2.57.1")
 
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
 }
